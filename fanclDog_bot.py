335883c02865038c8a8c36615601538ae9ddabd8
rutilant <lh.rutilant@gmail.com>
2018-08-30 15:37:21 +0800

    代码优化

--- fanclDog_bot.py
+++ fanclDog_bot.py
@@ -1,7 +1,10 @@
+#!/usr/bin/python3
 #coding=utf-8
+import argparse
 import logging
 import socket
 import time
+import yaml
 
 from telegram import (InlineKeyboardButton, InlineKeyboardMarkup,
                       InlineQueryResultArticle, InputTextMessageContent,
@@ -10,35 +13,28 @@ from telegram.ext import (CallbackQueryHandler, CommandHandler, Filters,
                           InlineQueryHandler, MessageHandler, Updater)
 from tinydb import Query, TinyDB
 
-TOKEN=''
-REQUEST_KWARGS={
-    'proxy_url': 'http://127.0.0.1:1080',
-}
-updater = Updater(TOKEN, request_kwargs=REQUEST_KWARGS)
-dispatcher = updater.dispatcher
+AUTHTOKEN = None
+REQUEST_KWARGS=None
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+logger = logging.getLogger(__name__)
 
+# y = open('./fanclDog.yaml', encoding='UTF-8')
+# rules = yaml.load(y)['rules']
 rule_table = TinyDB('rule_table.json')
 history = TinyDB('history.json')
 pointboard =TinyDB('pointboard.json')
 query_db = Query()
 
 def start(bot, update):
-    logging.info('收到指令/start')
-    bot.send_message(chat_id=update.message.chat_id, text="我是旺财，我是一个没有感情的尿床精。")
+    logger.info('收到指令/start')
+    bot.send_message(chat_id=update.message.chat_id, text='我是旺财，我是一个没有感情的尿床精。')
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-
-updater.start_polling()
 
 # 复读机功能
 def echo(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=update.message.text+"！")
+    bot.send_message(chat_id=update.message.chat_id, text=update.message.text+'！')
 
-echo_handler = MessageHandler(Filters.text, echo)
-dispatcher.add_handler(echo_handler)
 
 def build_menu(buttons,
                n_cols,
@@ -66,29 +62,26 @@ def show_cat_list(bot, update):
             button_list = [InlineKeyboardButton(ev['event'], callback_data=ev.doc_id)]
             button_lists.append(button_list)
             category[ev['category']] = len(button_lists)-1
-    logging.info('button lists created')
-    logging.info('categories:'+str(category))
+    logger.info('button lists created')
+    logger.info('categories:'+str(category))
     # 提取类别列表，返回类别给 callback query
     cat_button_list = []
     for cat in category.keys():
         cat_button_list.append(InlineKeyboardButton(cat, callback_data=str(1000+category.get(cat))))
-    logging.info('category button created')
+    logger.info('category button created')
     reply_markup = InlineKeyboardMarkup(build_menu(cat_button_list, n_cols=2))
-    bot.send_message(chat_id=update.message.chat_id, text="首先选择类别", reply_markup=reply_markup)
-
-point_handler = CommandHandler('done', show_cat_list)
-dispatcher.add_handler(point_handler)
+    bot.send_message(chat_id=update.message.chat_id, text='首先选择类别', reply_markup=reply_markup)
 
 
 def show_ev_list(bot,update,category):
     button_list = button_lists[category]
     reply_markup = InlineKeyboardMarkup(build_menu(button_list, n_cols=3))
-    bot.send_message(chat_id=update.effective_chat.id, text="你做了什么？", reply_markup=reply_markup)    
+    bot.send_message(chat_id=update.effective_chat.id, text='你做了什么？', reply_markup=reply_markup)    
 
 # callback 处理，菜单按钮反馈
 def point_up_action(bot,update):
     query = update.callback_query.data
-    logging.info('callback query received:'+query)
+    logger.info('callback query received:'+query)
     # print(query)
     if int(query) >= 1000:
         category = int(query)-1000
@@ -102,28 +95,25 @@ def point_up_action(bot,update):
             if mem['user_chat_id'] != current_chat_id:
                 bot.send_message(chat_id=mem['user_chat_id'], text=message)
 
-callback_handler = CallbackQueryHandler(point_up_action)
-dispatcher.add_handler(callback_handler)
-
 
 def add_point(update,eventid):
     userid = update.effective_user.id
     user_status = pointboard.get(query_db.userid == userid)
     event = rule_table.get(doc_id = eventid)
-    logging.info('current event:'+str(event))
-    if user_status != None:
+    logger.info('current event:'+str(event))
+    if user_status:  # is not none
         # 增加积分
         pointboard.update({'current_points': (user_status['current_points']+event['point']),'recent_change': event['point']}, query_db.userid == userid)
     else:
         # 添加新用户
-        logging.info('new user'+str(userid))
+        logger.info('new user'+str(userid))
         pointboard.insert({'userid':userid, 'username':update.effective_user.name, 'current_points':event['point'], 'recent_change':event['point'], 'user_chat_id':update.effective_chat.id, 'subscribe':False})
     # 写历史记录
     history.insert({'userid':userid,'event':event['event'],'change':event['point'], 'update_time':time.asctime( time.localtime(time.time()) )})
-    logging.info('history updated')
+    logger.info('history updated')
     # 读取改动过的记录
     user_status = pointboard.get(query_db.userid == userid)
-    logging.info('user updated:'+str(user_status))
+    logger.info('user updated:'+str(user_status))
     showtext = update.effective_user.name + ' 进行了' + event['event'] + '操作并获得了' + str(event['point']) + '点积分，目前的总积分是' + str(user_status['current_points']) + '。'
     return showtext
 
@@ -131,23 +121,58 @@ def add_point(update,eventid):
 def notification(bot,update):
     userid = update.effective_user.id
     user_status = pointboard.get(query_db.userid == userid)
-    if user_status['subscribe']:
+    if user_status['subscribe']:  # true
         bot.send_message(chat_id=update.effective_chat.id, text='已关闭订阅通知，现在你不会收到其他人的积分变动信息。')
     else:
         bot.send_message(chat_id=update.effective_chat.id, text='已开启订阅通知，现在你会收到所有人的积分变动信息。')
-        logging.info(userid+'is now a subscriber')
+        logger.info(userid+'is now a subscriber')
     pointboard.update({'subscribe':not user_status['subscribe']},query_db.userid == userid)
-sub_handler = CommandHandler('subscribe', notification)
-dispatcher.add_handler(sub_handler)
 
 
 # 对未知指令
 def unknown(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="喵喵喵？")
-
-unknown_handler = MessageHandler(Filters.command, unknown)
-dispatcher.add_handler(unknown_handler)
+    bot.send_message(chat_id=update.message.chat_id, text='喵喵喵？')
 
 
-updater.idle()
-# updater.stop()
+def startFromCLI():
+    global AUTHTOKEN, REQUEST_KWARGS
+    parser = argparse.ArgumentParser()
+    parser.add_argument('auth', type=str, help='The Auth Token given by Telegram''s @botfather')
+    parser.add_argument('-p','--proxy', type=str, default='http://127.0.0.1:1080', help='Proxy url used for http communication.')
+    parser.add_argument('-l','--llevel', default='info', choices=['debug','info','warn','none'], 
+                        help='Logging level for the logger, default = info')
+    
+    logLevel = {'none':logging.NOTSET,'debug':logging.DEBUG,'info':logging.INFO,'warn':logging.WARNING}
+    
+    args = parser.parse_args()
+    logger.setLevel(logLevel[args.llevel])
+        
+    AUTHTOKEN = args.auth
+    if args.proxy:
+        REQUEST_KWARGS = {
+            'proxy_url': args.proxy,
+        }
+
+
+def main():
+    updater = Updater(AUTHTOKEN, request_kwargs=REQUEST_KWARGS)
+    dispatcher = updater.dispatcher
+    # /start
+    dispatcher.add_handler(CommandHandler('start', start))
+    # /done
+    dispatcher.add_handler(CommandHandler('done', show_cat_list))
+    # /subscribe
+    dispatcher.add_handler(CommandHandler('subscribe', notification))
+    # callback
+    dispatcher.add_handler(CallbackQueryHandler(point_up_action))
+    # any text
+    dispatcher.add_handler(MessageHandler(Filters.text, echo))
+    # unknown command
+    dispatcher.add_handler(MessageHandler(Filters.command, unknown))
+    updater.start_polling()
+    updater.idle()
+    # updater.stop()
+
+if __name__ == '__main__':
+    startFromCLI()
+    main()
